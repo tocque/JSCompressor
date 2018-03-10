ed1bbc760682156681c74d490df60dd53f38b72f
oc <ckcz123@126.com>
2018-03-11 00:32:21 +0800

    v2.9

--- JSCompressor/Program.cs
+++ JSCompressor/Program.cs
@@ -17,6 +17,8 @@ namespace JSCompressor
             if (!File.Exists(directory + "main.js"))
                 directory = "..\\";
 
+            bool isV2 = Directory.Exists(directory + "project");
+
             string main="";
             try
             {
@@ -52,17 +54,55 @@ namespace JSCompressor
             }
             Console.WriteLine("------ 核心文件压缩完毕 ------\n\n");
 
+            string[] mapList = null;
+            if (isV2)
+            {
+                // find load list
+                index = main.IndexOf("this.pureData=[") + 15;
+                index2 = main.IndexOf("]", index);
+                loadlist = main.Substring(index, index2 - index).Replace("\"", "").Replace("'", "").Split(',');
+
+                Console.WriteLine("正在压缩项目文件...");
+                foreach (string one in loadlist)
+                {
+                    try
+                    {
+                        string data = File.ReadAllText(directory + "project\\" + one + ".js");
+                        data = compressor.Compress(data);
+                        File.WriteAllText(directory + "project\\" + one + ".min.js", data);
+                        Console.WriteLine("压缩中：project/" + one + ".js ===> project/" + one + ".min.js");
+
+                        if (one.Equals("data"))
+                        {
+                            index = data.IndexOf("floorIds:[")+10;
+                            index2 = data.IndexOf("]", index);
+                            mapList = data.Substring(index, index2 - index).Replace("\"", "").Replace("'", "").Split(',');
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(one + ".js 压缩出错：" + e.Message);
+                    }
+                }
+                Console.WriteLine("------ 项目文件压缩完毕 ------\n\n");
+            }
+
             Console.WriteLine("正在压缩地图文件...");
-            index = main.IndexOf("this.floorIds=[") + 15;
-            index2 = main.IndexOf("]", index);
-            string[] mapList = main.Substring(index, index2 - index).Replace("\"", "").Replace("'", "").Split(',');
+            if (mapList == null)
+            {
+                index = main.IndexOf("this.floorIds=[") + 15;
+                index2 = main.IndexOf("]", index);
+                mapList = main.Substring(index, index2 - index).Replace("\"", "").Replace("'", "").Split(',');
+            }
 
             string maps = "";
+            var floorDir = isV2 ? "project" : "libs";
             foreach (string map in mapList)
             {
                 try
                 {
-                    maps += compressor.Compress(File.ReadAllText(directory + "libs\\floors\\" + map + ".js"));
+                    maps += compressor.Compress(File.ReadAllText(directory + floorDir + "\\floors\\" + map + ".js"));
                     Console.WriteLine(map + ".js已压缩");
                 }
                 catch (Exception e)
@@ -70,8 +110,8 @@ namespace JSCompressor
                     Console.WriteLine("！！！！！！" + map + ".js 压缩出错：" + e.Message + "  请检查是否存在语法错误。");
                 }
             }
-            File.WriteAllText(directory + "libs\\floors.min.js", maps);
-            Console.WriteLine("======> 所有地图文件已压缩到libs/floors.min.js。");
+            File.WriteAllText(directory + floorDir + "\\floors.min.js", maps);
+            Console.WriteLine("======> 所有地图文件已压缩到" + floorDir + "/floors.min.js。");
             Console.WriteLine("------ 地图文件压缩完毕 ------\n\n");
 
             Console.WriteLine("=====================================================");
